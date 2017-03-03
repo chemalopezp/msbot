733ee2d4d228235a69844254499a9d13b074102e
Chema Lopez <chema.lopezp@gmail.com>
2017-03-02 16:04:08 -0800

    commit from vscode

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
-     }
-})
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+// var server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+//    console.log('%s listening to %s', server.name, server.url);
+// });
+
+// Create chat bot
+var consoleConnector = new builder.ConsoleConnector().listen();
+// var connector = new builder.ChatConnector({
+//     appId: process.env.MICROSOFT_APP_ID,
+//     appPassword: process.env.MICROSOFT_APP_PASSWORD
+// });
+var bot = new builder.UniversalBot(consoleConnector);
+// server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
